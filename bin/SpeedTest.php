b62b6b98c3cc6b7b952e03801216e0755a0a839f
Fernando Aquino <nandico@gmail.com>
2014-01-12 20:04:23 -0200

    First version of upstream test.

--- bin/SpeedTest.php
+++ bin/SpeedTest.php
@@ -1,13 +1,19 @@
 <?php
+/*
 if ($_FILES["file"]["error"] > 0)
   {
-  echo "Error: " . $_FILES["file"]["error"] . "<br>";
+  echo "Error: " . $_FILES["file"]["error"] . "<br>\n";
   }
 else
   {
-  echo "Upload: " . $_FILES["file"]["name"] . "<br>";
-  echo "Type: " . $_FILES["file"]["type"] . "<br>";
-  echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
+  echo "Upload: " . $_FILES["file"]["name"] . "<br>\n";
+  echo "Type: " . $_FILES["file"]["type"] . "<br>\n";
+  echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>\n";
   echo "Stored in: " . $_FILES["file"]["tmp_name"];
   }
+ */
+	$postdata = file_get_contents("php://input");
+	echo strlen( $postdata );
+
+
 ?>
